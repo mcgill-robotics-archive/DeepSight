73115e83969d0d6238fada2e91922f6600c21347
aalexsmithh <alexsmith1@me.com>
2017-01-14 21:57:19 -0500

    started new load code

---
+++ load_data.py
@@ -0,0 +1,12 @@
+import numpy as nu
+
+def load(path_to_data):
+	#takes a string pointing to a location that contains an img folder with raw images, 
+	#and a label folder that contains the labels and bounding boxes as text files for each frame
+	return loaded_data
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
