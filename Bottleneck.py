e7016b26b54140e2bec97b83a631531dc3650016
GabrielAlacchi <alacchi.g@gmail.com>
2017-01-14 23:17:05 -0500

    Added bottleneck generation method

--- Bottleneck.py
+++ Bottleneck.py
@@ -4,6 +4,7 @@ import lasagne
 import numpy as np
 from os import path, listdir
 
+from load_data import load_image
 
 class Bottleneck:
 
@@ -27,3 +28,12 @@ class Bottleneck:
         input_files = [path.join(self.bottleneck_dir, f) for f in listdir(self.bottleneck_dir) if f.endswith('.npy')]
 
         return map(lambda fpath: {"path": fpath, "data": np.load(fpath)}, input_files)
+
+    def generate_bottlenecks(self, data_root):
+    	image_dir = path.join(data_root, 'img')
+    	image_files = [path.join(image_dir, f) for f in listdir(image_dir) if f.endswith('.jpg')]
+
+    	for image_file in image_files:
+    		image_array = load_image(image_file, flat=False)
+    		name = path.basename(image_array).split('.')[0]
+    		self.save_bottleneck(name, input_tensor=image_array)
